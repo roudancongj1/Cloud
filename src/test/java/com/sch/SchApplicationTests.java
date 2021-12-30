c6c1b947ae55af08df6ad2200f896f3cace9fef6
Gao <Yun_mic@126.com>
2021-12-31 00:26:17 +0800

    逆向工程

--- src/test/java/com/sch/SchApplicationTests.java
+++ src/test/java/com/sch/SchApplicationTests.java
@@ -8,6 +8,19 @@ class SchApplicationTests {
 
     @Test
     void contextLoads() {
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/data","root","123")
+                .globalConfig(builder -> {
+                    builder.author("Gao") // 设置作者
+                            .outputDir("D://_____idea//_____autoscan"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("sacn"); // 设置父包名
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("juxing") // 设置需要生成的表名
+                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                })
+                .execute();
     }
 
 }
