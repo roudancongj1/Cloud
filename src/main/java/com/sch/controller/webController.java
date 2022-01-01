11956ec1850ed2723d5d0b096daceb2a3cb2acb3
Gao <Yun_mic@126.com>
2022-01-01 22:14:38 +0800

    界面

--- src/main/java/com/sch/controller/webController.java
+++ src/main/java/com/sch/controller/webController.java
@@ -2,6 +2,7 @@ package com.sch.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @Auth: Gao
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class webController {
     @RequestMapping("/")
-    public String index(){
-        return "index";
+    public String index() { return "page/index.html"; }
+    @RequestMapping("404")
+    public String error(){
+        return "page/404.html";
     }
 }
