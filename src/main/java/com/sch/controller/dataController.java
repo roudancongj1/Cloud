648fc2b07ac3bca1fd0cd165b0ed28ba1b9fd475
Gao <Yun_mic@126.com>
2022-01-11 01:31:08 +0800

    工具类

--- src/main/java/com/sch/controller/dataController.java
+++ src/main/java/com/sch/controller/dataController.java
@@ -1,6 +1,8 @@
 package com.sch.controller;
 
+import com.sch.utils.resultUtil;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,39 @@ public class dataController {
         map.put("info","啊大苏打");
         map.put("name","张三");
         return map;
+    }
+    @GetMapping("cardInfo")
+    public List<Map> cardInfo(){
+        Map map1 = new HashMap();
+        map1.put("label","优秀的团队");
+        map1.put("service","训练有素的服务");
+        Map map2 = new HashMap();
+        map2.put("label","和善的");
+        map2.put("service","大苏打撒旦");
+        Map map3 = new HashMap();
+        map3.put("label","阿斯顿萨达");
+        map3.put("service","3232");
+        Map map4 = new HashMap();
+        map4.put("label","嗷嗷嗷");
+        map4.put("service","顶顶顶顶顶");
+        List list = new ArrayList();
+        list.add(map1);
+        list.add(map2);list.add(map3);
+        list.add(map4);
+
+
+        return list;
+    }
+    @RequestMapping("result")
+    public resultUtil result(){
+       // return resultUtil.ok().put("asdasdaa");
+       // return resultUtil.ok().put("2232","dsadsa");
+
+      //  return resultUtil.ok().put("asdas","dsadsa");
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("asdas","sad");
+        map.put("22","sad");
+        map.put("asd33as","sad");
+        return resultUtil.ok().put(map);
     }
 }
---
+++ src/main/java/com/sch/utils/resultUtil.java
@@ -0,0 +1,64 @@
+package com.sch.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Auth: Gao
+ * @Date: 2022/1/11 0:32
+ */
+public class resultUtil extends HashMap<String,Object> {
+
+    //默认成功
+    resultUtil(){
+        put("code","0");
+        put("msg","success");
+    }
+
+
+    resultUtil(Integer code,Object msg){
+        put("code",code);
+        put("msg",msg);
+    }
+    //成功
+    public static resultUtil ok(){
+        return new resultUtil();
+    }
+    //成功
+    public static resultUtil ok(String msg){
+        return new resultUtil(0,msg);
+    }
+    //成功
+    public static resultUtil ok(Integer code,String msg){
+        return new resultUtil(code,msg);
+    }
+    //失败
+    public static resultUtil error(){
+        return new resultUtil(500,"操作失败");
+    }
+    //失败
+    public static resultUtil error(String msg){
+        return new resultUtil(500,msg);
+    }
+    //失败
+    public static resultUtil error(Integer code,String msg){
+        return new resultUtil(code,msg);
+    }
+
+    @Override
+    public resultUtil put(String key, Object value) {
+        super.put(key, value);
+        return this;
+    }
+
+    public resultUtil put(Object data){
+        super.put("data",data);
+        return this;
+    }
+    //return的this指向当前方法返回对象
+    public resultUtil put(Map<String,Object> map){
+        putAll(map);
+        return this;
+    }
+
+}
