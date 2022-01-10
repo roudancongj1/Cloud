f498c87b2837b172d17c4d2f93693e6fbdd28df1
Gao <Yun_mic@126.com>
2022-01-10 18:30:16 +0800

    雏形

--- src/main/java/com/sch/controller/webController.java
+++ src/main/java/com/sch/controller/webController.java
@@ -1,6 +1,7 @@
 package com.sch.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -24,4 +25,6 @@ public class webController {
     public String account(){ return "page/account.html"; }
     @RequestMapping("manage")
     public String manage(){ return "page/manage.html"; }
+    @GetMapping("*")
+    public String intercept(){ return "redirect:404"; }
 }
