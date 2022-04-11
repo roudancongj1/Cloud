02d8d08ffecd1101759c749c748bc53815f028e3
Gao <Yun_mic@126.com>
2022-04-11 16:11:50 +0800

    验证码发送

--- src/main/java/com/sch/controller/noaspect/LoginController.java
+++ src/main/java/com/sch/controller/noaspect/LoginController.java
@@ -2,13 +2,11 @@ package com.sch.controller.noaspect;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.sch.dao.UserMapper;
 import com.sch.pojo.RegFrom;
 import com.sch.pojo.TokenEntity;
 import com.sch.pojo.User;
 import com.sch.pojo.UserForm;
-import com.sch.service.MailService;
 import com.sch.utils.RedisUtil;
 import com.sch.utils.ResultUtil;
 import com.sch.utils.TokenUtil;
@@ -21,7 +19,6 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
 
--- src/main/java/com/sch/utils/TokenUtil.java
+++ src/main/java/com/sch/utils/TokenUtil.java
@@ -45,3 +45,4 @@ public class TokenUtil {
         return stringBuilder.toString();
     }
 }
+
